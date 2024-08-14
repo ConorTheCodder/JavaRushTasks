624490c86f45398e3fcdffc79a3dc314f6ed4651
Hamid Musayev <cherryducker@gmail.com>
2024-08-14 23:46:35 +0400

    almost finished level 2

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 5;
+        int numericValue = 656;
+        int size = 5454;
+        int number = 45;
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "gdfgdf";
+        String phrase = "dfgdfgdfg";
+        String line = "gegfddfgfdgdfg";
+        String text = "gdfgfgnjjfgkjjfgjfhjg";
+
+
+    }
+}
