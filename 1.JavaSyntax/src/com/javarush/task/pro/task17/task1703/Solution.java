6ff32a03a16f040d518125c246908d9f3c408a48
Hamid Musayev <cherryducker@gmail.com>
2024-11-13 03:21:22 +0400

    Finished level 18

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task17.task1703;
+
+import java.util.ArrayList;
+
+/* 
+Космическая одиссея ч.1
+*/
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+    }
+
+    public static void createCrew(){
+        Astronaut humanOne = new Human();
+        Astronaut humanTwo = new Human();
+        Astronaut dog = new Dog();
+        Astronaut cat = new Cat();
+
+        astronauts.add(humanOne);
+        astronauts.add(humanTwo);
+        astronauts.add(cat);
+        astronauts.add(dog);
+
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
